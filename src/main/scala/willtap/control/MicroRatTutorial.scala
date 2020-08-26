1268a22070ab3320d32962262b317cdda42eb620
William Billingsley <wbillingsley@cantab.net>
2020-08-26 22:16:13 +1000

    Started adding levels and video to MicroRat tutorial

--- src/main/scala/willtap/control/MicroRatTutorial.scala
+++ src/main/scala/willtap/control/MicroRatTutorial.scala
@@ -8,9 +8,9 @@ import com.wbillingsley.veautiful.templates.Challenge
 import com.wbillingsley.veautiful.templates.Challenge.{Complete, Level, Open}
 import org.scalajs.dom
 import org.scalajs.dom.{Element, Node, html}
-import willtap.templates.{ExerciseStage, VNodeStage}
+import willtap.templates.{ExerciseStage, MarkdownStage, VNodeStage, YouTubeStage}
 import willtap._
-import willtap.imperativeTopic.PrefabCodable
+import willtap.imperativeTopic.{JellyFlood, PrefabCodable}
 
 import scala.util.Random
 
@@ -25,25 +25,25 @@ object MicroRatTutorial {
 
   implicit val onCompletionUpdate: () => Unit = () => challenge.rerender()
 
-  def mdTask(markdown:String, mazeString:String, tw:Int = 5, th:Int = 5, start:(Int, Int) = (0, 0), inaccurate:Boolean = true)(f: dom.CanvasRenderingContext2D => Unit) = new ExerciseStage() {
+  def mdTask(markdown:String, mazeString:String, tw:Int = 10, th:Int = 10, start:(Int, Int) = (0, 0), inaccurate:Boolean = true) = new ExerciseStage() {
 
     override def completion: Challenge.Completion = Open
 
     private val (rx, ry) = start
 
-    val maze = new MicroRat({ sim =>
+    val maze = new MicroRat()({ sim =>
       sim.loadMazeFromString(mazeString)
+
     }, { sim => })
 
     val codable = JSCodable(CanvasLand("exercise")(
       fieldSize= (tw * MicroRat.ONE_TILE_PIXELS) -> (th * MicroRat.ONE_TILE_PIXELS),
       r = maze.robot,
       setup = c => {
-        c.fillCanvas("white")
-        c.drawGrid("rgb(200,240,240)", RescueLine.tileSize, 1)
-        c.withCanvasContext { ctx =>
-          f(ctx)
-        }
+        c.fillCanvas("black")
+        maze.paintCanvas(c)
+        c.addSteppable(maze.Goal)
+        c.addSteppable(maze)
       }
     ))(tilesMode = false)
 
@@ -54,7 +54,7 @@ object MicroRatTutorial {
 
 
   def smallMaze(s:String, tw:Int = 5, th:Int = 2, mazeString:String, start:(Int, Int) = (0, 0), inaccurate:Boolean = true, largeInaccurate:Boolean=false)(f: dom.CanvasRenderingContext2D => Unit) = {
-    val maze = new MicroRat({ sim =>
+    val maze = new MicroRat(dimensions = (tw, th), start=start)({ sim =>
       sim.loadMazeFromString(mazeString)
     }, { sim => })
 
@@ -110,8 +110,7 @@ object MicroRatTutorial {
            |There's a little code on the right to make it bounce back and forth off the walls, but that's just to
            |illustrate that the walls are solid.
            |
-           |We're going to have a lot to show you before you can get this little guy solving mazes, so let's start
-           |with how on earth we steer it if all we can do is set the power and direction of the motors...
+           |We're going to have a lot to show you before you can get this little guy solving mazes...
            |""".stripMargin),
         () => VNodeStage.card(PrefabCodable(
           """function go() {
@@ -139,10 +138,117 @@ object MicroRatTutorial {
           smallMaze("Hello world", 8, 3,
             """########
               |#......#
-              |########
-              |""".stripMargin, (1,1), inaccurate = false) { ctx =>
+              |########""".stripMargin, (1,1), inaccurate = false) { ctx =>
           }
         ))
+      ),
+      YouTubeStage("dhWUOp5wOgU"),
+      mdTask(
+        """In a moment, we'll keep going, writing some turn and move functions. But before we do, just have a play
+          |with Bumper. A little challenge: make Bumper drive right, but turn the tail light green after it gets
+          |halfway. The tiles are 64 pixels wide, so the 6th tile starts at x coordinate 320. You can set the tail
+          |light colour with `setLightColour("green")` and get the `x` position with `getX()`
+          |""".stripMargin,
+        """..........
+          |.###..###.
+          |.#......#.
+          |.#.####.#.
+          |...#......
+          |......#...
+          |.#.####.#.
+          |.#......#.
+          |.###..###.
+          |..........
+          |""".stripMargin
+      )
+    )),
+    Level(name = "Turning Bumper", stages = Seq(
+      MarkdownStage(() =>
+        """## Video to come...
+          |""".stripMargin),
+      mdTask(
+        """Now it's your turn. Try implementing the turn function. Then see what angles you need to turn it to to
+          |hit the various wall blocks. You might find they're a little far away - Bumper's turn won't be totally
+          |accurate. That's going to be ok, because in the maze we'll only be steering him to destinations one
+          |square away (before we work out where we want to turn to next).
+          |
+          |Hint: Once you've done this, copy your code and save it in a text file. You'll need it for the next stage.
+          |""".stripMargin,
+        """.........#
+          |..........
+          |..........
+          |..........
+          |..........
+          |.........#
+          |..........
+          |..........
+          |..........
+          |#....#...#
+          |""".stripMargin
+      )
+    )),
+    Level(name = "Moving Bumper", stages = Seq(
+      MarkdownStage(() =>
+        """## Video to come...
+          |""".stripMargin),
+      mdTask(
+        """Your move this time. Implement the move function, and see if you can steer bumper through this fixed maze.
+          |
+          |In the challenge we're building up to, you're not allowed to hard-code the route or give Bumper "foreknowledge
+          |of the maze". For the moment, though, let's just test our move function works
+          |
+          |Hint: Once you've done this, copy your code and save it in a text file. You'll need it for the next stage.
+          |""".stripMargin,
+        """..........
+          |.###..###.
+          |.#......#.
+          |.#.####.#.
+          |...#......
+          |......#...
+          |.#.####.#.
+          |.#......#.
+          |.###..###.
+          |..........
+          |""".stripMargin
+      )
+    )),
+    Level(name = "Mapping and Routefinding", stages = Seq(
+      VNodeStage.twoColumn("Remember our Jelly Flood")(() => Common.markdown(
+        s"""
+           |Let's go back to the flood fill algorithm, where we imagine pouring some liquid jelly onto the goal.
+           |
+           |After zero turns, it's only on the goal square.  \t
+           |After one turn, it'd move to the neighbouring squares.  \t
+           |After two turns, it'd move to their neighbours.  \t
+           |
+           |If we count how long it takes to reach a square, that's the *distance from the goal*.
+           |We can then use this in an algorithm - we move to any square that has a lower goal distance than our own.
+           |
+           |That's a route-finding algorithm if we know where the wall are. Curiously, it's works as an exploration
+           |algorithm when we *don't* know where the walls are...
+           |
+           |""".stripMargin),
+        () => VNodeStage.card(
+          JellyFlood()
+        )
+      ),
+    )),
+    Level(name = "ASSIGNMENT", stages = Seq(
+      VNodeStage.twoColumn("Assignment Levels")(() => Common.markdown(
+        s"""
+           |In the challenge levels, you need to program Bumper to map and solve the maze, go back to the start, and
+           |then race to the middle again.
+           |
+           |So it's more visible what Bumper is doing:
+           |
+           |* When Bumper is exploring and mapping the maze, set the tail-light blue
+           |* When Bumper is returning to the start square, set it red
+           |* When Bumper is racing to the middle, set it green
+           |
+           |There are a few maps for you to try your program on. You should use the same program for Bumper, we've just
+           |given you a few different mazes to try it out on.
+           |""".stripMargin),
+        () => <.div()
       ),
     )),
   )
